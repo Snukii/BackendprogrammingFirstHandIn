716a3c2a684468c5a0ef3b97d46b98803ed7e961
Snukii <ssnnuukkii@gmail.com>
2018-09-17 19:28:05 +0200

    Add project files.

---
+++ firsthandin/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firsthandin
+{
+    class Customer
+    {
+        private string firstName = "Carsten";
+        private string lastName = "Andersen";
+        private int age = 38;
+        private int zipcode = 4800;
+        private string password = "VeryGoodPassword123";
+
+        public Customer()
+        {
+
+        }
+
+        public Customer(string _firstName, string _lastName, int _age, int _zipcode, string _password)
+        {
+            firstName = _firstName;
+            lastName = _lastName;
+            age = _age;
+            zipcode = _zipcode;
+            password = _password;
+        }
+
+        public void ResetPassword(string _password)
+        {
+            password = _password;
+        }
+
+        public override string ToString()
+        {
+            return $"Customer data: \n Name: {firstName} {lastName} \n Age: {age} \n Zipcode: {zipcode} \n Password: {password}";
+        }
+
+    }
+}
---
+++ firsthandin/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firsthandin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer standard = new Customer();
+            Console.WriteLine("Default " + standard + "\n");
+
+            Customer custom = new Customer("Jan", "Rasmussen", 74, 6800, "123123123");
+            Console.WriteLine("Custom " + custom + "\n");
+
+            custom.ResetPassword("987987987");
+
+            Console.WriteLine("Custom " + custom + "\n");
+        }
+    }
+}
